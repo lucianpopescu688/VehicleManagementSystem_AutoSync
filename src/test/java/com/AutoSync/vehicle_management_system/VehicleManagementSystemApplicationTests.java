ecd7b434c2c9597ba7297a1df5758a51ecf74a4e
lucianpopescu688 <lucianpopescu688@gmail.com>
2026-04-28 17:06:30 +0200

    Initial commit: Spring Boot Gradle setup for AutoSync

---
+++ src/test/java/com/AutoSync/vehicle_management_system/VehicleManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AutoSync.vehicle_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
